bc49a0966eb933bca49e9d6e6762e090cba94dd5
Guido Lorenz <guido.lorenz@artcom.de>
2014-05-07 16:46:51 +0200

    Configure deployment path

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,12 @@
 lock '3.1.0'
 
 set :application, 'hoccer-receiver'
-set :deploy_to, "/var/www/#{fetch(:application)}"
+set :runner, 'receiver'
+set :deploy_to, "/home/#{fetch :runner}/#{fetch :application}"
 
 set :scm, :jenkins
-set :repo_url, "https://jenkins.hoccer.de/job/#{fetch(:application)}"
+set :repo_url, "https://jenkins.hoccer.de/job/#{fetch :application}"
 set :jenkins_artifact_file, "target/hoccer-receiver-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 
 before 'deploy', 'jenkins:load_secrets'
+before 'deploy:starting', 'deploy:create_deploy_path_with_correct_permissions'
