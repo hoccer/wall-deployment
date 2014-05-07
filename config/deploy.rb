844bd9f057351365037b4bfb291346fbc8adf029
Guido Lorenz <guido.lorenz@artcom.de>
2014-05-07 16:52:26 +0200

    Normalize quotes

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, "/home/#{fetch :runner}/#{fetch :application}"
 
 set :scm, :jenkins
 set :repo_url, "https://jenkins.hoccer.de/job/#{fetch :application}"
-set :jenkins_artifact_file, "target/hoccer-receiver-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+set :jenkins_artifact_file, 'target/hoccer-receiver-0.0.1-SNAPSHOT-jar-with-dependencies.jar'
 
 before 'deploy', 'jenkins:load_secrets'
 before 'deploy:starting', 'deploy:create_deploy_path_with_correct_permissions'
