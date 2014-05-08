dade7b927a557442fc6ec27a89f770ef0b4a771e
Guido Lorenz <guido.lorenz@artcom.de>
2014-05-08 17:09:18 +0200

    Create receiver configuration file
    The file is created from a template and filled with the actual
    values that can be defined for every server in the stage configuration.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,8 +12,11 @@ set :jenkins_artifact_file, 'target/hoccer-receiver-0.0.1-SNAPSHOT-jar-with-depe
 set :jenkins_insecure, true
 
 before 'deploy', 'jenkins:load_secrets'
+
 before 'deploy:starting', 'deploy:create_deploy_path_with_correct_permissions'
 after 'deploy:starting', 'deploy:fix_linked_dirs_permissions'
+
+after 'deploy:updating', 'config:create'
 
 before 'deploy:publishing', 'upstart:prepare_script'
 after 'deploy:published', 'upstart:restart'
--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,1 +1,6 @@
-server '127.0.0.1:2222', user: 'deployment', roles: ['app']
+server '127.0.0.1:2222', user: 'deployment', roles: ['app'], config: {
+  server: 'wss://test1.talk.hoccer.de',
+  avatar_file: './res/football.jpg',
+  latitude: 50.948613,
+  longitude: 6.908733
+}
