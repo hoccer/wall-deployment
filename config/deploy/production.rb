5977103ab32c22dbb57307ccb439d389d0062a46
Guido Lorenz <guido.lorenz@artcom.de>
2014-05-19 17:59:51 +0200

    Configure production stage

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,6 @@
-server '172.20.10.14', roles: ['jar']
+server 'receiver1.talk.hoccer.de', user: 'deployment', roles: ['app'], config: {
+  talk_server: 'wss://whitelabel-test1.talk.hoccer.de:8443',
+  avatar_file: './res/football.jpg',
+  latitude: 50.948613,
+  longitude: 6.908733
+}
