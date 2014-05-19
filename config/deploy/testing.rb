c082e8934abc366097100a99432f9d420d8dc179
Guido Lorenz <guido.lorenz@artcom.de>
2014-05-19 16:29:35 +0200

    Rename server setting to talk_server

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,5 +1,5 @@
 server '127.0.0.1:2222', user: 'deployment', roles: ['app'], config: {
-  server: 'wss://test1.talk.hoccer.de',
+  talk_server: 'wss://test1.talk.hoccer.de',
   avatar_file: './res/football.jpg',
   latitude: 50.948613,
   longitude: 6.908733
