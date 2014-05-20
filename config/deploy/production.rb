cb307f970349dddfc5d4ed21302c8c439d9e1472
Guido Lorenz <guido.lorenz@artcom.de>
2014-05-20 17:44:47 +0200

    Configure receiver contact name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 server 'receiver1.talk.hoccer.de', user: 'deployment', roles: ['app'], config: {
   talk_server: 'wss://whitelabel-test1.talk.hoccer.de:8443',
+  contact_name: 'the wall',
   avatar_file: 'football.jpg',
   latitude: 50.948613,
   longitude: 6.908733
--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,5 +1,6 @@
 server '127.0.0.1:2222', user: 'deployment', roles: ['app'], config: {
   talk_server: 'wss://test1.talk.hoccer.de',
+  contact_name: 'the wall',
   avatar_file: 'football.jpg',
   latitude: 50.948613,
   longitude: 6.908733
