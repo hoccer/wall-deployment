87b70c1f7d0a9c8ee99b39c05cdf3892d32c904e
Guido Lorenz <guido.lorenz@artcom.de>
2014-05-20 17:42:25 +0200

    Add avatar image support

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server 'receiver1.talk.hoccer.de', user: 'deployment', roles: ['app'], config: {
   talk_server: 'wss://whitelabel-test1.talk.hoccer.de:8443',
-  avatar_file: './res/football.jpg',
+  avatar_file: 'football.jpg',
   latitude: 50.948613,
   longitude: 6.908733
 }
