2c171f3cb51788ccd19b5e7125dc9811db679d2d
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-18 15:30:55 +0200

    Update stage configurations

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
-server 'receiver1.talk.hoccer.de', user: 'deployment', roles: ['app'], config: {
+server 'receiver.talk.hoccer.de', user: 'deployment', roles: ['app'], config: {
   talk_server: 'wss://talkserver-test1.talk.hoccer.de:8443',
-  backend_port: 80,
-  contact_name: 'the wall',
+  backend_port: 8080,
+  contact_name: 'wall (production)',
   avatar_file: 'football.jpg',
   latitude: 50.948613,
   longitude: 6.908733
--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,7 +1,7 @@
 server '127.0.0.1:2222', user: 'deployment', roles: ['app'], config: {
   talk_server: 'wss://talkserver-test1.talk.hoccer.de:8443',
   backend_port: 5000,
-  contact_name: 'the wall',
+  contact_name: 'wall (testing)',
   avatar_file: 'football.jpg',
   latitude: 50.948613,
   longitude: 6.908733
