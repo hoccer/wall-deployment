61210a0e95a7af8b78ca91012fb0af00896119d2
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-22 15:11:28 +0200

    Change backend port on production system
    (to match provisioned nginx config)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server 'wall.talk.hoccer.de', user: 'deployment', roles: ['app'], config: {
   talk_server: 'wss://talkserver-test1.talk.hoccer.de:8443',
-  backend_port: 8080,
+  backend_port: 5000,
   contact_name: 'wall (production)',
   avatar_file: 'football.jpg',
   latitude: 50.948613,
