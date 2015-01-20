82fa86a57568f9e55dc252c407924db7cb155a3f
Guido Lorenz <guido.lorenz@artcom.de>
2015-01-20 17:19:04 +0100

    Fix webclient-backend deployment

--- services/webclient-backend/config/deploy.rb
+++ services/webclient-backend/config/deploy.rb
@@ -8,6 +8,7 @@ set :linked_dirs, ['log', 'encrypted_attachments', 'decrypted_attachments', 'dat
 set :scm, :jenkins
 set :repo_url, "https://jenkins.hoccer.de/job/hoccer-talk-develop"
 set :jenkins_artifact_file, "#{fetch :application}/target/#{fetch :application}-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+set :deployed_artifact_filename, "#{fetch :application}.jar"
 set :jenkins_insecure, true
 
 before 'deploy', 'jenkins:load_secrets'
